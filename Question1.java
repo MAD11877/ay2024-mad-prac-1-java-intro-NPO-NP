9f2a467b82026343da7d3cc4827475f7cc42d1af
NPO-NP <78456508+NPO-NP@users.noreply.github.com>
2024-04-14 18:46:32 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,7 +2,8 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    System.out.println("Hey!")
+    System.out.println("Hey!");
+    System.out.println("Hello world!");
     /**
      * Print "Hello World!"
      * 
